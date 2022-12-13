229bb11af9475643d4e2a17f0764e94343a4f734
Brian Sánchez González <bsanchez53@alumnos.uaq.mx>
2022-12-12 23:56:01 -0600

    DOM

---
+++ index.js
@@ -0,0 +1,55 @@
+
+//1)Al hacer click en el botón, cambia el color de cada texto y además el nombre del color
+ /* function changeColor(){
+    document.getElementById("t1").style.color = "red";
+    t1.innerText='Text red';
+    document.getElementById("t2").style.color = "gray";
+    t2.innerText='Text gray';
+    document.getElementById("t3").style.color = "pink";
+    t3.innerText='Text pink';
+    
+} */
+
+
+// --------------------------------------------------------------------
+
+// 2)Al hacer click en el botón, convierte el cuadro a un círculo
+
+/* function Magiccircle(){
+    var circ = document.querySelector('#circulo');
+    circ.style.borderRadius='100%';
+   
+}*/
+
+// --------------------------------------------------------------------
+
+// 3)Al hacer click en el botón, cambia el color de aquellos exs pares
+
+/*function ChangeColor(){
+
+    var colors = ['green','orange','pink']
+    var color1 = Math.floor(Math.random()* colors.length);
+    var par0 = document.getElementsByClassName('par0');
+    var ex1 = par0[0];
+    ex1.style.background = colors[color1];
+
+    var color2 = Math.floor(Math.random()* colors.length);
+    var par1 = document.getElementsByClassName('par1');
+    var ex2 = par1[0];
+    ex2.style.background = colors[color2];
+
+    var color3 = Math.floor(Math.random()* colors.length);
+    var par2 = document.getElementsByClassName('par2');
+    var ex3 = par2[0];
+    ex3.style.background = colors[color3];
+}*/
+
+        // var colores=["white", "AliceBlue", "AntiqueWhite", "Aqua", "DarkSeaGreen", "DarkTurquoise"];
+        // var body=document.querySelector("body");
+        // body.style="background-color:"+colores[Math.floor(Math.random() * colores.length)];
+        
+
+       
+
+         
+        
